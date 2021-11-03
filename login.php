1ac29d3e0743fa0640bb61a87b4dc8d9435d09a7
gentaprima <gentaprima600@gmail.com>
2021-11-03 16:55:54 +0700

    update login

--- login.php
+++ login.php
@@ -6,8 +6,16 @@ $personalNumber = isset($_POST['personal_number']) ? $_POST['personal_number'] :
 $password = isset($_POST['password']) ? $_POST['password'] : '';
 $data = readDataPerRow($conn, "SELECT id,personal_number,name FROM users where personal_number = '$personalNumber' AND password='$password'");
 header("Content-Type: application/json");
-echo json_encode([
-    "data" => empty($data) ? [] : $data,
-    "status" =>  empty($data) ? false : true,
-    "message" => empty($data) ?    "Personal Number Tidak Sesuai !" : "Berhasil Login"
-]);
+if(empty($data)){
+
+    echo json_encode([
+        "status" =>  empty($data) ? false : true,
+        "message" => empty($data) ?    "Personal Number Tidak Sesuai !" : "Berhasil Login"
+    ]);
+}else{
+    echo json_encode([
+        "data" => empty($data) ? [] : $data,
+        "status" =>  empty($data) ? false : true,
+        "message" => empty($data) ?    "Personal Number Tidak Sesuai !" : "Berhasil Login"
+    ]);
+}
